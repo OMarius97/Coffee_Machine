b1773c0111de91348cb8b187a3338d050bf25e4d
OMarius97 <mariusoana1997@gmail.com>
2022-04-03 22:58:33 +0300

    .

---
+++ main.py
@@ -0,0 +1,21 @@
+from menu import Menu, MenuItem
+from coffee_maker import CoffeeMaker
+from money_machine import MoneyMachine
+
+meniu = Menu()
+Coffee_Machine = CoffeeMaker()
+money = MoneyMachine()
+
+while True:
+    chose = input(f"What would you like? {meniu.get_items()} ").lower()
+    if chose == "espresso" or chose == "latte" or chose == "cappuccino":
+        drink = meniu.find_drink(chose)
+        if Coffee_Machine.is_resource_sufficient(drink) and money.make_payment(drink.cost):
+                Coffee_Machine.make_coffee(drink)
+
+
+    elif chose == "off":
+        pass
+    elif chose == "report":
+        Coffee_Machine.report()
+        money.report()
